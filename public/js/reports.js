2716c8ce43a4289adcb3fdb5772cbcb6f3ba249a
Simon Baev <simonbaev@gmail.com>
2017-03-25 23:27:38 -0400

    chart added on reports view

--- public/js/reports.js
+++ public/js/reports.js
@@ -1,15 +1,95 @@
 /* jshint esnext: true */
 
+function termCodeToString(termCode) {
+	let year = termCode.slice(0,4);
+	let semester = {'02':'Spring', '05':'Summer', '08':'Fall'}[termCode.slice(4)];
+	return semester + ' ' + year;
+}
+function courseToString(course) {
+	return course.split(/_/).join(' ');
+}
+
+
 $(document).ready(function() {
+	let chartData = JSON.parse(document.querySelector('#chart').dataset.chart);
+	console.log(chartData);
+	let term = '201702';
+	let course = 'CSCI_4940';
+	let project = 'MT';
+	if(chartData &&
+		chartData[term] &&
+		chartData[term][course] &&
+		chartData[term][course][project] &&
+		chartData[term][course][project].students) {
+		let displayData = chartData[term][course][project].students;
+		//-- Breadcrumb
+		$('.breadcrumb')
+		.append($('<li>').append($('<a>').attr({'href':'#'}).text('Reports')))
+		.append($('<li>').append($('<a>').attr({'href':'#'}).text(termCodeToString(term))))
+		.append($('<li>').append($('<a>').attr({'href':'#'}).text(courseToString(course))))
+		.append($('<li>').addClass('active').text(project));
+		if(Object.keys(displayData).length === 1) {
+			//-- Student's view with only one e-mail
+			let student = displayData[Object.keys(displayData)[0]];
+			let name = student.name;
+			let labelInterpolationFnc = function(value) {
+				let parts = value.split(/\s+/);
+				return parts[0][0] + parts[1];
+			};
+			if (student.scores.labels.length) {
+				new Chartist.Bar(
+					'.ct-chart',
+					{
+						labels: student.scores.labels.map(function(label, index, array) {
+							return label || "Student " + (index + 1);
+						}),
+						series: [student.scores.criteria.calculated]
+					},
+					{
+						reverseData: true,
+						horizontalBars: true,
+						seriesBarDistance: 20,
+						axisY: {
+							labelInterpolationFnc: labelInterpolationFnc
+						},
+						axisX: {
+							labelOffset: {
+								x: -10,
+								y: 0
+							},
 
-	var data = {
-		// A labels array that can contain any sort of values
-		labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-		// Our series array that contains series objects or in this case series data arrays
-		series: [
-			[5, 2, 4, 2, 0]
-		]
-	};
-	new Chartist.Line('.ct-chart', data);
-
+						}
+					},
+					[
+						[
+							'screen and (min-width: 450px)', {
+								reverseData: false,
+								horizontalBars: false,
+								axisY: {
+									type: Chartist.FixedScaleAxis,
+									labelInterpolationFnc: Chartist.noop,
+									ticks: [0,10,20,30,40,50,60,70,80,90,100]
+								},
+								axisX: {
+									// type: Chartist.StepAxis,
+									labelInterpolationFnc: labelInterpolationFnc,
+									labelOffset: {
+										x: 0,
+										y: 0
+									},
+								}
+							}
+						],
+					]
+				);
+			}
+		}
+	}
+	else {
+		$('.reports-body').empty().append(
+			$('<h3>')
+			.addClass('text-info')
+			.text('No report data found')
+		);
+	}
 });
--- routes/reports.js
+++ routes/reports.js
@@ -11,54 +11,77 @@ router.get('/', function (req, res, next) {
 		return res.redirect('/login');
 	}
 	User.findById(req.session.uid, function(err, user){
-		if(err) {
+		if(err || !user) {
 			return res.render('reports', {
 				status: 1,
 				message: 'Cannot retrieve user details',
 				session: req.session
 			});
 		}
-		let data = {
-			term: '201702',
-			courses: {},
-		};
-
+		let data = {};
 		Contribution.find().lean().exec(function(err,contributions){
 			if(err) {
 				return res.render('reports', {
 					status: 2,
-					message: 'Cannot retrieve contribution data'
+					message: 'Cannot retrieve contribution data',
+					session: req.session
 				});
 			}
 			for(let contribution of contributions) {
-				if(!data.courses[contribution.course]) {
-					data.courses[contribution.course] = {
-						projects: {}
-					};
+				if(!data[contribution.term]) {
+					data[contribution.term] = {};
 				}
-				let course = data.courses[contribution.course];
-				if(!course.projects[contribution.project]) {
-					course.projects[contribution.project] = {
+				let term = data[contribution.term];
+				if(!term[contribution.course]) {
+					term[contribution.course] = {};
+				}
+				let course = term[contribution.course];
+				if(!course[contribution.project]) {
+					course[contribution.project] = {
 						title: contribution.title,
 						students: {}
 					};
 				}
-				let project = course.projects[contribution.project];
+				let project = course[contribution.project];
 				for(let student of contribution.members){
-					if(!project.students[student.email]) {
-						project.students[student.email] = {
-							name: student.name || 'Student',
-							topic: contribution.topic,
-							scores: []
-						};
+					if(user.faculty || user.email === student.email) {
+						if(!project.students[student.email]) {
+							project.students[student.email] = {
+								name: student.name || 'Student',
+								topic: contribution.topic,
+								scores: {
+									labels: [],
+									criteria: {
+										presentation: [],
+										progress: [],
+										overall: [],
+										calculated: []
+									}
+								}
+							};
+						}
+						let scores = project.students[student.email].scores;
+						scores.labels.push(user.faculty ? contribution.email : null);
+						scores.criteria.presentation.push(student.presentation);
+						scores.criteria.progress.push(student.progress);
+						scores.criteria.overall.push(contribution.overall);
+						scores.criteria.calculated.push(contribution.overall / 100 * (student.progress * 0.5 + student.presentation * 0.5));
 					}
-					project.students[student.email].scores.push({
-						contributor: contribution.email,
-						overall: contribution.overall,
-						progress: student.progress,
-						presentation: student.presentation,
-						calculated: contribution.overall / 100 * (student.progress * 0.5 + student.presentation * 0.5)
-					});
+				}
+			}
+			for(let term in data) {
+				for(let course in data[term]) {
+					for(let project in data[term][course]) {
+						if(!Object.keys(data[term][course][project].students).length) {
+							delete data[term][course][project];
+						}
+					}
+					if(!Object.keys(data[term][course]).length) {
+						delete data[term][course];
+					}
+				}
+				if(!Object.keys(data[term]).length) {
+					delete data[term];
 				}
 			}
 			res.render('reports', {
