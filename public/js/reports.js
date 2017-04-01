6184dbfc0d7f9b64ae29663b14d0156901cfc0d8
Simon Baev <simonbaev@gmail.com>
2017-03-31 23:35:31 -0400

    individual report OK

--- public/js/reports.js
+++ public/js/reports.js
@@ -7,82 +7,158 @@ socket
 })
 .on('data', function(data){
 	console.log(data);
-	//-- Depending on the number of terms in data we display either a a list of terms or jump to "the only one"
-	if(Object.keys(data).length === 1) {
-		$('.breadcrumb')
+	//-- Set term select
+	let termData = data;
+	let termKeys = Object.keys(data);
+	let termSelect = $('#select-term').empty();
+	for(let term of termKeys) {
+		termSelect
 		.append(
-			$('<li>')
-			.addClass('bc-reports')
-			.append(
-				$('<a>')
-				.attr({
-					'href': '/reports',
-				})
-				.text('Reports')
-			)
+			$('<option>')
+			.attr({
+				'value': term
+			})
+			.text(termData[term].title)
 		);
-		termClickHandler.call(data[Object.keys(data)[0]]);
 	}
-	else {
-		$('.breadcrumb')
-		.append(
-			$('<li>')
-			.addClass('active')
-			.text('Reports')
-		);
-		$('.selector')
-		.empty()
-		.append($('<fieldset>').append($('<legend>').text('Please select the term')))
-		.append($('<ul>').addClass('list-unstyled'));
-		for(let term in data) {
-			$('.selector ul')
+	termSelect.find('option:eq(0)').attr('selected','');
+	termSelect.change(function(){
+		//-- Set course within the selected term
+		let option = $(this).find('option:selected');
+		let courseData = termData[option.val()].courses;
+		let courseKeys = Object.keys(courseData);
+		let courseSelect = $('#select-course').empty();
+		for(let course of courseKeys) {
+			courseSelect
 			.append(
-				$('<li>')
-				.append(
-					$('<a>')
-					.text(data[term].title)
-					.attr({
-						'href': '#',
-						'data-term': term
-					})
-					.click(termClickHandler.bind(data[term]))
-				)
+				$('<option>')
+				.attr({
+					'value': course
+				})
+				.text(course.toUpperCase().split(/_/).join(' ') + ' ' + courseData[course].title)
 			);
 		}
-	}
+		courseSelect.find('option:eq(0)').attr('selected','');
+		courseSelect.change(function(){
+			//-- Set project within the selected course
+			let option = $(this).find('option:selected');
+			let projectData = courseData[option.val()].projects;
+			let projectKeys = Object.keys(projectData);
+			let projectSelect = $('#select-project').empty();
+			for(let project of projectKeys) {
+				projectSelect
+				.append(
+					$('<option>')
+					.attr({
+						'value': project
+					})
+					.text(projectData[project].title)
+				);
+			}
+			projectSelect.find('option:eq(0)').attr('selected','');
+			projectSelect.change(function(){
+				//-- Update the chart or table
+				let option = $(this).find('option:selected');
+				let displayData = projectData[option.val()].students;
+				let displayKeys = Object.keys(displayData);
+				let container = $('#report-details').empty();
+				if(displayKeys.length === 1) {
+					//-- Student's case, render a chart
+					//-= Prepare container
+					container
+					.append(
+						$('<div>')
+						.addClass('flex-center')
+						.append($('<h3>').text(displayData[displayKeys[0]].topic.title))
+					)
+					.append(
+						$('<div>')
+						.addClass('ct-chart ct-my')
+					)
+					.append(
+						$('<dl>')
+						.addClass('dl-horizontal')
+						.append($('<dt>').text('Average:'))
+						.append($('<dd>').attr('id','stat-average'))
+						.append($('<dt>').text('Standard deviation:'))
+						.append($('<dd>').attr('id','stat-deviation'))
+					);
+					//-- Prepare and fill chart data
+					let chartData = displayData[displayKeys[0]].contributions;
+					let chartKeys = Object.keys(chartData);
+					let chartLabels = chartKeys.map(function(entry,index,array){
+						return 'S' + (index+1);
+					});
+					let chartSeries = chartKeys.map(function(entry,index,array){
+						return chartData[entry];
+					});
+					//-- Calculate and display statistics
+					let average = chartSeries.reduce(function(acc,value){
+						return acc + value;
+					}, 0) / chartSeries.length;
+					let deviation = Math.sqrt(chartSeries.map(function(entry){
+						return Math.pow(entry - average, 2);
+					})
+					.reduce(function(acc,value){
+						return acc + value;
+					}, 0) / chartSeries.length);
+					$('#stat-average').text(average.toFixed(2));
+					$('#stat-deviation').text(deviation.toFixed(2));
+					//-- Populate chart with series data
+					$('.ct-chart').empty();
+					new Chartist.Bar(
+						'.ct-chart',
+						{
+							labels: chartLabels,
+							series: [chartSeries]
+						},
+						{
+							reverseData: true,
+							horizontalBars: true,
+							seriesBarDistance: 20,
+							axisX: {
+								labelOffset: {
+									x: -10,
+									y: 0
+								},
+							}
+						},
+						[
+							[
+								'screen and (min-width: 450px)', {
+									reverseData: false,
+									horizontalBars: false,
+									axisY: {
+										type: Chartist.FixedScaleAxis,
+										labelInterpolationFnc: Chartist.noop,
+										ticks: [0,10,20,30,40,50,60,70,80,90,100]
+									},
+									axisX: {
+										labelOffset: {
+											x: 0,
+											y: 0
+										},
+									}
+								}
+							],
+						]
+					);
+				}
+				else {
+					//-- Faculty case, render a summary table
+
+				}
+			})
+			.trigger('change');
+		})
+		.trigger('change');
+	})
+	.trigger('change');
+
 });
 
-function termClickHandler() {
-	let data = this;
-	let subData = data.courses;
-	let subDataKeys = Object.keys(subData);
-	console.log(data);
-	if(subDataKeys.length === 1) {
-		if($('.bc-term').length) {
-			$('.bc-reports').nextAll().remove();
-		}
-		$('.breadcrumb')
-		.append(
-			$('<li>')
-			.addClass('bc-term')
-			.append(
-				$('<a>')
-				.attr({
-					'href': '#',
-				})
-				.click(courseClickHandler.bind(data))
-				.text(data.title)
-			)
-		);
-		courseClickHandler(subData[subDataKeys[0]].projects);
-	}
-	else {
+function setSelect(data,select) {
 
-	}
-}
-function courseClickHandler() {
-	let data = this;
-	console.log(data);
 }
 
 $(document).ready(function() {
